502a783e3b0855c8033b0f2f6a99bd89c91028a3
Šimon Kvíčala <simon.kvicala@ibacz.eu>
2021-06-25 14:56:21 +0200

    Installer initial commit

--- MidPointCommonTaskModels/ClassExtensions/EnvironmentHelper.cs
+++ MidPointCommonTaskModels/ClassExtensions/EnvironmentHelper.cs
@@ -12,6 +12,8 @@ namespace Common
     {
         public const string loggingHive = @"SOFTWARE\ADPasswordFilter";
 
+        #region Agent Settings
+
         public static Int64 GetAgentLogging()
         {
             Int64 value;
@@ -56,6 +58,10 @@ namespace Common
 
             return value;
         }
+
+        #endregion
+
+        #region MidpointUpdatingService Settings
 
         public static string GetMidpointBaseUrl()
         {
@@ -183,49 +189,6 @@ namespace Common
             return value;
         }
 
-
-        public static string GetQueueFolder()
-        {
-            string value = null;
-            try
-            {
-                //get the 64-bit view first
-                RegistryKey key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
-                key = key.OpenSubKey(loggingHive);
-
-                if (key == null)
-                {
-                    //we couldn't find the value in the 64-bit view so grab the 32-bit view
-                    key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
-                    key = key.OpenSubKey(loggingHive);
-                }
-
-                if (key != null)
-                {
-                    value = key.GetValue("QueueFolder").ToString();
-                }
-                else
-                {
-                    using (EventLog eventLog = new EventLog("Application"))
-                    {
-                        eventLog.Source = "ADPasswordAgent";
-                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder - using default folder."), EventLogEntryType.Warning, 201, 1);
-                    }
-
-                }
-            }
-            catch (Exception ex)
-            {
-                using (EventLog eventLog = new EventLog("Application"))
-                {
-                    eventLog.Source = "ADPasswordAgent";
-                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
-                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder - using default folder."), EventLogEntryType.Warning, 201, 1);
-                }
-            }
-            return value;
-        }
-
         public static Int64 GetRetryCount()
         {
             Int64 value;
@@ -265,50 +228,6 @@ namespace Common
                     eventLog.Source = "ADPasswordAgent";
                     eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\RetryCount : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
                     eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\RetryCount - using default 50 retries."), EventLogEntryType.Warning, 201, 1);
-                }
-            }
-            return value;
-        }
-
-        public static Int64 GetQueueWaitSeconds()
-        {
-            Int64 value;
-            try
-            {
-                //get the 64-bit view first
-                RegistryKey key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
-                key = key.OpenSubKey(loggingHive);
-
-                if (key == null)
-                {
-                    //we couldn't find the value in the 64-bit view so grab the 32-bit view
-                    key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
-                    key = key.OpenSubKey(loggingHive);
-                }
-
-                if (key != null)
-                {
-                    value = Convert.ToInt64(key.GetValue("QueueWaitSeconds").ToString());
-                }
-                else
-                {
-                    value = 30;
-                    using (EventLog eventLog = new EventLog("Application"))
-                    {
-                        eventLog.Source = "ADPasswordAgent";
-                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds - using default 30 seconds."), EventLogEntryType.Warning, 201, 1);
-                    }
-
-                }
-            }
-            catch (Exception ex)
-            {
-                value = 30;
-                using (EventLog eventLog = new EventLog("Application"))
-                {
-                    eventLog.Source = "ADPasswordAgent";
-                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
-                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds - using default 30 seconds."), EventLogEntryType.Warning, 201, 1);
                 }
             }
             return value;
@@ -358,7 +277,93 @@ namespace Common
             return value;
         }
 
-        
+        public static string GetMidpointServiceLogPath()
+        {
+            string value;
+            try
+            {
+                //get the 64-bit view first
+                RegistryKey key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+                key = key.OpenSubKey(loggingHive);
+
+                if (key == null)
+                {
+                    //we couldn't find the value in the 64-bit view so grab the 32-bit view
+                    key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
+                    key = key.OpenSubKey(loggingHive);
+                }
+
+                if (key != null)
+                {
+                    value = key.GetValue("MidpointServiceLogPath").ToString();
+                }
+                else
+                {
+                    value = @"Logs\";
+                    using (EventLog eventLog = new EventLog("Application"))
+                    {
+                        eventLog.Source = "ADPasswordAgent";
+                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath - using default path .\Logs."), EventLogEntryType.Warning, 201, 1);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                value = @"Logs\";
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "ADPasswordAgent";
+                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
+                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath - using default path .\Logs."), EventLogEntryType.Warning, 201, 1);
+                }
+            }
+            return value;
+        }
+
+        public static Int64 GetMidpointSsl()
+        {
+            Int64 value;
+            try
+            {
+                //get the 64-bit view first
+                RegistryKey key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+                key = key.OpenSubKey(loggingHive);
+
+                if (key == null)
+                {
+                    //we couldn't find the value in the 64-bit view so grab the 32-bit view
+                    key = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
+                    key = key.OpenSubKey(loggingHive);
+                }
+
+                if (key != null)
+                {
+                    value = Convert.ToInt64(key.GetValue("MidpointSSL").ToString());
+                }
+                else
+                {
+                    value = 0;
+                    using (EventLog eventLog = new EventLog("Application"))
+                    {
+                        eventLog.Source = "ADPasswordAgent";
+                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL - using HTTP connection"), EventLogEntryType.Warning, 205, 1);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                value = 0;
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "ADPasswordAgent";
+                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL : {0}", ex.Message), EventLogEntryType.Error, 305, 1);
+                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL - using HTTP connection"), EventLogEntryType.Warning, 205, 1);
+                }
+            }
+            return value;
+        }
 
         public static string GetMidpointCertName()
         {
@@ -404,11 +409,13 @@ namespace Common
             return value;
         }
 
+        #endregion
 
+        #region Common Queue Settings
 
-        public static string GetMidpointServiceLogPath()
+        public static string GetQueueFolder()
         {
-            string value;
+            string value = null;
             try
             {
                 //get the 64-bit view first
@@ -424,34 +431,31 @@ namespace Common
 
                 if (key != null)
                 {
-                    value = key.GetValue("MidpointServiceLogPath").ToString();
+                    value = key.GetValue("QueueFolder").ToString();
                 }
                 else
                 {
-                    value = @"Logs\";
                     using (EventLog eventLog = new EventLog("Application"))
                     {
                         eventLog.Source = "ADPasswordAgent";
-                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath - using default path .\Logs."), EventLogEntryType.Warning, 201, 1);
+                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder - using default folder."), EventLogEntryType.Warning, 201, 1);
                     }
 
                 }
             }
             catch (Exception ex)
             {
-                value = @"Logs\";
                 using (EventLog eventLog = new EventLog("Application"))
                 {
                     eventLog.Source = "ADPasswordAgent";
-                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
-                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointServiceLogPath - using default path .\Logs."), EventLogEntryType.Warning, 201, 1);
+                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
+                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueFolder - using default folder."), EventLogEntryType.Warning, 201, 1);
                 }
             }
             return value;
         }
 
-
-        public static Int64 GetMidpointSsl()
+        public static Int64 GetQueueWaitSeconds()
         {
             Int64 value;
             try
@@ -469,30 +473,32 @@ namespace Common
 
                 if (key != null)
                 {
-                    value = Convert.ToInt64(key.GetValue("MidpointSSL").ToString());
+                    value = Convert.ToInt64(key.GetValue("QueueWaitSeconds").ToString());
                 }
                 else
                 {
-                    value = 0;
+                    value = 30;
                     using (EventLog eventLog = new EventLog("Application"))
                     {
                         eventLog.Source = "ADPasswordAgent";
-                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL - using HTTP connection"), EventLogEntryType.Warning, 205, 1);
+                        eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds - using default 30 seconds."), EventLogEntryType.Warning, 201, 1);
                     }
 
                 }
             }
             catch (Exception ex)
             {
-                value = 0;
+                value = 30;
                 using (EventLog eventLog = new EventLog("Application"))
                 {
                     eventLog.Source = "ADPasswordAgent";
-                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL : {0}", ex.Message), EventLogEntryType.Error, 305, 1);
-                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\MidpointSSL - using HTTP connection"), EventLogEntryType.Warning, 205, 1);
+                    eventLog.WriteEntry(String.Format(@"Error accessing registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds : {0}", ex.Message), EventLogEntryType.Error, 302, 1);
+                    eventLog.WriteEntry(String.Format(@"Warning - unable to read registry key HKEY_LOCAL_MACHINE\SOFTWARE\ADPasswordFilter\QueueWaitSeconds - using default 30 seconds."), EventLogEntryType.Warning, 201, 1);
                 }
             }
             return value;
         }
+
+        #endregion
     }
 }
