6760ba1f953564e5a13568d4b0902f5fcf863a34
Martin Záruba <martin.zaruba@artin.eu>
2022-05-19 17:32:23 +0200

    IDM-2 - inc. version to 1.0.0.3

--- MidPointUpdatingService/Properties/AssemblyInfo.cs
+++ MidPointUpdatingService/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // pomocí zástupného znaku * takto:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
